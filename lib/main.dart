6ab39ee7f154efd083867c3ceeedfce54d40143b
FDSAP-LACHESIS <fdsap-lachesis@Intern5s-MacBook-Air.local>
2023-05-11 16:58:47 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const FAQScreen(),
+      debugShowCheckedModeBanner: false,
+      home: const FaqPage(),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:faqscreen/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const FAQScreen());
+    await tester.pumpWidget(const FaqPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
